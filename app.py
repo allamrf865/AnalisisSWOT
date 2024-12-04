96af9ef31d4e15133a970f5530de7c8413556558
allamrf865 <allamrf865@gmail.com>
2024-12-05 01:29:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -383,8 +383,7 @@ if st.button("Analyze"):
             # Display LSI and Interpretation
             st.subheader(f"Leadership Viability Index (LSI): {lsi:.2f}")
             st.write(f"**Interpretation**: {lsi_interpretation}")
-
-            def generate_and_display_charts(swot_scores):
+def generate_and_display_charts(swot_scores):
     # Paths to save the charts
     heatmap_path = "/tmp/heatmap.png"
     scatter_chart_path = "/tmp/scatter_plot.png"
